f4bb4ae435e0815506ba9ede595a30182a162140
jmancipet <jmancipet@ucentral.edu.co>
2024-11-24 21:20:05 -0500

    Commits finales

--- app/src/main/java/com/example/gorunner/MapsActivity.java
+++ app/src/main/java/com/example/gorunner/MapsActivity.java
@@ -3,7 +3,6 @@ package com.example.gorunner;
 import androidx.fragment.app.FragmentActivity;
 
 import android.database.Cursor;
-import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -43,6 +42,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 null, RecorridosObtenidos.recorridoId + " = " + idViaje, null, null);
 
         PolylineOptions linea = new PolylineOptions().clickable(false);
+        linea.color(0xff0000ff);
         LatLng primeraLoc = null;
         LatLng ultimaLoc = null;
         try {
@@ -63,7 +63,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         float zoom = 17.0f;
         if(ultimaLoc != null && primeraLoc != null) {
-            // Marcadores de inicio y fin
+
             mapa.addMarker(new MarkerOptions()
                     .position(primeraLoc)
                     .title("Inicio")
@@ -75,17 +75,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .title("Fin")
                     .icon(BitmapDescriptorFactory.fromResource(R.drawable.fin))); // Icono personalizado
 
-
             mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(primeraLoc, zoom));
-
-            // Dibujar línea entre el punto inicial y el punto final con un color diferente
-            PolylineOptions lineaInicioFin = new PolylineOptions()
-                    .add(primeraLoc, ultimaLoc) // Agrega los puntos inicial y final
-                    .width(10) // Define el ancho de la línea
-                    .color(Color.BLUE) // Cambia el color de la línea
-                    .clickable(false);
-            mapa.addPolyline(lineaInicioFin);
         }
+        mapa.addPolyline(linea);
     }
-
 }
